b7f998e675906a4e82e737a6932930dcb497121f
Okunola Orogun <adebola.orogun@aaua.ed.ng>
2024-05-23 13:09:12 +0100

    updated

---
+++ src/components/data_transformation.py
@@ -0,0 +1,51 @@
+import sys 
+from dataclasses import dataclass
+import os
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.logger import logging
+from src.exception import CustomException
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        try:
+            numerical_features = ["writing_score", "reading_score"]
+            categorical_columns = [
+                "gender", "race_ethnicity",
+                "parental_level_of_education",
+                "lunch","test_preparation_course"
+            ]
+
+            num_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="median")),
+                    ("scaler", StandardScaler())
+                ]
+            )
+
+            cat_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder", OneHotEncoder()),
+                    ("scaler", StandardScaler())
+                ]
+
+            )
+            logging.info("Numerical columns scaling completed")
+            logging.info("Categorical columns encoding completed")
+        except:
+            pass
+    
